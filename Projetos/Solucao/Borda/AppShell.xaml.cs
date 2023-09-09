66723c37c90d46d64972c317033633714fdbe671
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-09-09 14:00:50 -0300

    S04E02 - Usando Border no MAUI

---
+++ Projetos/Solucao/Borda/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Borda
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projetos/Solucao/Borda/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Borda
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
