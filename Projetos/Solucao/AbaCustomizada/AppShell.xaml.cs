178329a13b29352053daf9d92d39152f04cd39bf
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-09-14 18:14:06 -0300

    S08E01 - Criando o Projeto e Design - Parte 1

---
+++ Projetos/Solucao/AbaCustomizada/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AbaCustomizada
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projetos/Solucao/AbaCustomizada/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AbaCustomizada
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
