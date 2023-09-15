c2f32783a075c7cd7cf3b5e9c0c975289c305944
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-09-15 12:10:31 -0300

    S09E03 - ControlTemplate - Parte 3

---
+++ Projetos/Solucao/Templates/ViewModels/MeuTemplateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Templates.ViewModels
+{
+    public partial class MeuTemplateViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private void VerTermos()
+        {
+            App.Current.MainPage.DisplayAlert("Clicou!", "Clicou!", "OK");
+        }
+    }
+}
