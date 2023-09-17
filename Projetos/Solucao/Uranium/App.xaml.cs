a0c471c86dbd02f47b1c8b84d19fe2cd0fc28b0c
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-09-17 10:36:26 -0300

    Configurando o UraniumUI

---
+++ Projetos/Solucao/Uranium/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Uranium
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Projetos/Solucao/Uranium/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Uranium
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
